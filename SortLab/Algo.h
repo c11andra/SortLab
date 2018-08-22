c249f8287e9094954ae346a57a3453ed21c65d0c
c11andra <c11andra@github.com>
2018-08-22 14:25:45 +0530

    Implement BubbleSort Optimized

--- SortLab/Algo.h
+++ SortLab/Algo.h
@@ -1,7 +1,7 @@
 #ifndef ALGO_H
 #define ALGO_H
 #include <iostream>
-using namespace std;
+
 template <class T>
 class Algo
 {
@@ -19,6 +19,27 @@ class Algo
                                         array[j] = temp;
                                 }
                         }       
+                }
+        }
+
+        static void BubbleSortOptimized(T* array, int count)
+        {
+                bool sorted = true;
+                for(int i = count - 1; i >= 0 ; --i)
+                {
+                        sorted = false;
+                        for(int j = 1; j <= i ; ++j)
+                        {
+                                if(array[j-1] > array[j])
+                                {
+                                        T temp = array[j-1];
+                                        array[j-1] = array[j];
+                                        array[j] = temp;
+                                        sorted = true;
+                                }
+                        }       
+                        if(sorted == false) break;
+                        
                 }
         }
 };
--- SortLab/main.cpp
+++ SortLab/main.cpp
@@ -12,9 +12,12 @@ void PrintArray(int* arr, int count)
 
 int main(int argc, char **argv)
 {
-    int arr[] = {1, 10, 2, 20, 5, 3, -9};
+    int arr[] = {1, 10, 2, 20, 5, 3, 4, 5};
+
     int count = sizeof(arr)/sizeof(arr[0]);
-    Algo<int>().BubbleSort(arr, count);
+   // Algo<int>::BubbleSort(arr, count);
+
+    Algo<int>::BubbleSortOptimized(arr, count);
 
     PrintArray(arr, count);
     return 0;
